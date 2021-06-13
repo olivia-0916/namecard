80c3652798b7283ea749bab8ad20c0480df2527f
olivia-0916 <yuxuanlin599@gmail.com>
2021-06-14 00:49:20 +0800

    add namecard foundation

---
+++ namecard.sec/namecard2.0/Namecard.swift
@@ -0,0 +1,14 @@
+//
+//  Namecard.swift
+//  namecard2.0
+//
+//  Created by 麥可不可 on 2021/6/14.
+//
+
+import Foundation
+struct Namecard: Decodable {
+    var Name: String
+    var Job: String
+    var Company: String
+    var Image: String
+}
