02568d4aa51f645a9536ba708511fe8ecd31f0cc
olivia-0916 <yuxuanlin599@gmail.com>
2021-06-14 20:03:11 +0800

    detail page outlet erro

--- namecard.sec/namecard2.0/cardcollecTableViewCell.swift
+++ namecard.sec/namecard2.0/cardcollecTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class cardcollectionTableViewCell: UITableViewCell {
+class cardcollecTableViewCell: UITableViewCell {
 
     @IBOutlet weak var imageview: UIImageView!
     @IBOutlet weak var namelabel: UILabel!
---
+++ namecard.sec/namecard2.0/collecDetailpageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  collectionDetailpageViewController.swift
+//  namecard2.0
+//
+//  Created by 麥可不可 on 2021/6/14.
+//
+
+import UIKit
+
+class collecDetailpageViewController: UIViewController {
+
+    
+    @IBOutlet weak var namelabel: UILabel!
+    
+    var namecard: Namecard?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        namelabel.text = namecard?.Name
+        
+        
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+}
+    
+}
