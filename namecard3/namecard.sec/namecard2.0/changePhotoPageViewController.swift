7445b909e8a25c2b07122d4b59c6f050e9e4a391
HsiaoJou <105202052@nccu.edu.tw>
2021-06-18 01:48:05 +0800

    pick photo func and alert

--- namecard3/namecard.sec/namecard2.0/changePhotoPageViewController.swift
+++ namecard3/namecard.sec/namecard2.0/changePhotoPageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class changePhotoPageViewController: UIViewController {
+class changePhotoPageViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     @IBOutlet weak var photoImageView: UIImageView!
     
@@ -17,7 +17,38 @@ class changePhotoPageViewController: UIViewController {
 
     }
     
+    func libraryPicker(){
+        let photoController = UIImagePickerController()
+        photoController.delegate = self
+        photoController.sourceType = .photoLibrary
+        present(photoController, animated: true, completion: nil)
+    }
+    func camera(){
+        let cameraController = UIImagePickerController()
+        cameraController.delegate = self
+        cameraController.sourceType = .camera
+        present(cameraController, animated: true, completion: nil)
+    }
+   // func imagePickerController(_ picker: UIImagePickerController,)}
+    
+    //點擊
     @IBAction func pickPhotoImageView(_ sender: UITapGestureRecognizer) {
+        let controller = UIAlertController(title: "選擇相片", message: nil, preferredStyle: .actionSheet)
+        //相機
+        let cameraAction = UIAlertAction(title:"開啟相機", style: .default){(_) in
+            self.camera()
+        }
+        //相簿
+        let libraryAction = UIAlertAction(title: "從相簿選擇", style: .default){(_) in
+            self.libraryPicker()
+        }
+        //取消
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        
+        controller.addAction(cameraAction)
+        controller.addAction(libraryAction)
+        controller.addAction(cancelAction)
+        present(controller, animated: true, completion: nil)
     }
     
     /*
