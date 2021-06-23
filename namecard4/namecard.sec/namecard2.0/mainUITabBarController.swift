0c5e7ce4f0e4fa5d90c2a6e9657c3fc1169479ec
曾意晴 <105209038@g.nccu.edu.tw>
2021-06-23 22:01:27 +0800

    storybo修

--- namecard4/namecard.sec/namecard2.0/mainUITabBarController.swift
+++ namecard4/namecard.sec/namecard2.0/mainUITabBarController.swift
@@ -17,11 +17,6 @@ class mainUITabBarController: UITabBarController {
 //        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
 //        tabBar.standardAppearance = appearance
         
-//        let mycard = mycardViewController()
-//        let mycardController = UINavigationController(rootViewController: mycard)
-//        mycardController.tabBarItem.image = UIImage(named: "card")
-        
-        
         //self.tabBar.tintColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
         
         UITabBar.appearance().tintColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
