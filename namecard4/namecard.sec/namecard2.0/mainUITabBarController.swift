ce60b497a03ce6260de98c4a6cc7732011b43e57
曾意晴 <105209038@g.nccu.edu.tw>
2021-06-23 21:36:40 +0800

    tabiconcolor解決

--- namecard4/namecard.sec/namecard2.0/mainUITabBarController.swift
+++ namecard4/namecard.sec/namecard2.0/mainUITabBarController.swift
@@ -12,7 +12,20 @@ class mainUITabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tabBar.tintColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
+//        let appearance = tabBar.standardAppearance
+//
+//        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
+//        tabBar.standardAppearance = appearance
+        
+//        let mycard = mycardViewController()
+//        let mycardController = UINavigationController(rootViewController: mycard)
+//        mycardController.tabBarItem.image = UIImage(named: "card")
+        
+        
+        //self.tabBar.tintColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
+        
+        UITabBar.appearance().tintColor = UIColor(red: 70/255, green: 119/255, blue: 138/255, alpha: 1)
+        
         
         
 
