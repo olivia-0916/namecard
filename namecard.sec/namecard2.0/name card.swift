917163ddc8b8d3754e46290a5bbc5af7a6fad10e
olivia-0916 <yuxuanlin599@gmail.com>
2021-06-27 21:56:17 +0800

    刪除舊的

--- namecard.sec/namecard2.0/name card.swift
+++
@@ -1,8 +0,0 @@
-//
-//  name card.swift
-//  namecard2.0
-//
-//  Created by 麥可不可 on 2021/6/14.
-//
-
-import Foundation
--- namecard/namecard/othernamecard.swift
+++
@@ -1,8 +0,0 @@
-//
-//  othernamecard.swift
-//  namecard
-//
-//  Created by 麥可不可 on 2021/6/8.
-//
-
-import Foundation
